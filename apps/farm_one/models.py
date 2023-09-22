d34d0dac7c83811e53dec0672dc25d2542ecba15
Faishal003 <1801003@iot.bdu.ac.bd>
2023-09-22 07:28:18 +0600

    farmone

---
+++ apps/farm_one/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+
+from utils.mixin import BaseModelMixin
+
+
+# Create your models here.
+
+class FarmOne(BaseModelMixin):
+    """
+    Farm One Database Schema
+    """
+    ph = models.FloatField(name="ph", null=True, blank=True)
+    temperature = models.FloatField(name="temperature", null=True, blank=True)
+    turbidity = models.FloatField(name="turbidity", null=True, blank=True)
+    is_active = models.BooleanField(name="is_active", default=True)
+
+    def __str__(self):
+        return "ID: " + str(self.id) + ", PH: " + str(self.ph) + ", TEMPERATURE: " + str(self.temperature) + ", TURBIDITY: " + str(self.turbidity)
+
+
+
+class Subject(BaseModelMixin):
+    """
+    Subject Database Schema
+    """
+    name = models.CharField(name="name", max_length=256, null=True, blank=True)
+    code = models.CharField(name="code", max_length=256, null=True, blank=True)
+    is_active = models.BooleanField(name="is_active", default=True)
+
+    def __str__(self):
+        return "ID: " + str(self.id) + ", NAME: " + str(self.name) + ", CODE: " + str(self.code)
+
+
+class FarmoneSubject(BaseModelMixin):
+    """
+    FarmoneSubject Database Schema
+    """
+    farmone = models.ForeignKey(FarmOne, on_delete=models.CASCADE, null=True, blank=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True, blank=True)
+    is_active = models.BooleanField(name="is_active", default=True)
+
+    def __str__(self):
+        return "ID: " + str(self.id) + ", FARMONE: " + str(self.farmone) + ", SUBJECT: " + str(self.subject)
+
+
---
+++ apps/farm_one/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+from .views import FarmOneViewSet
+
+app_name = "farm_one"
+router = DefaultRouter()
+router.register(r"", FarmOneViewSet, basename="students")
+urlpatterns = [
+    path("", include(router.urls)),
+    # path("students/", StudentsViewSet.as_view(), name="students"),
+]
